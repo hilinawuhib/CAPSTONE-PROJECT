4a2e3e6e1acc58c6839f31231f9e0f17ea1057b1
hilina88 <hilinawuhib@gmail.com>
2022-05-02 18:57:25 +0200

    Update index.js

--- edukids-app/src/redux/store/index.js
+++ edukids-app/src/redux/store/index.js
@@ -2,14 +2,9 @@ import { createStore, combineReducers, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import userReducer from "../reducers/userReducer";
 const aComposeFunction = window._REDUX_DEVROOLS_EXTENSION_COMPOSE__ || compose;
+const user=JSON.parse(localStorage.getItem("user"))
 export const initialState = {
-  user: {
-    first_name: "",
-    last_name: "",
-    email: "",
-    password: "",
-    role: "",
-  },
+  user: user ? user : null
 };
 const bigReducer = combineReducers({
   user: userReducer,
